401f2d8455a0f3b01ae75ef126153683bc0503a2
Ikmal-coder <ikmalzaki00@gmail.com>
2024-10-23 15:54:14 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MSI-Week-2
+# WEEK 2 DIGITAL LOGIC SYSTEM GROUP 3
+
+| Matric Number | Name                            |
+| ------------- | ------------------------------- |
+| 2215359       | ARIF EMRULLAH BIN TAJUL ARIFFIN |
+| 2210620       | AZLIYANA SYAHIRAH BINTI AZAHARI |
+| 2217830       | DAMIA MAISARAH BINTI ZAWAWI     |
+| 2217830       | HUDA BINTI AB RAHMAN AL-QARI    |
+| 2125625       | IKMAL HAKIM BIN ZAKI            |
+
+![Working 7 Segment LED with Button](image.jpg)
+
+https://github.com/user-attachments/assets/6a22ce2e-abae-4dfb-8314-24c7627e0b0d
+
---
+++ Week_2_G3_LEDSegment.ino
@@ -0,0 +1,62 @@
+#define BUTTON_PIN 10
+int segmentA = 2;
+int segmentB = 3;
+int segmentC = 4;
+int segmentD = 5;
+int segmentE = 6;
+int segmentF = 7;
+int segmentG = 8;
+
+int i = 0;
+int j = 0;
+
+int Arduino_Pins[7] = {segmentA, segmentB, segmentC, segmentD, segmentE, segmentF, segmentG};
+int Segment_Display[10][7] = {{0, 0, 0, 0, 0, 0, 1}, //display number 0
+                              {1, 0, 0, 1, 1, 1, 1}, //display number 1
+                              {0, 0, 1, 0, 0, 1, 0}, //display number 2
+                              {0, 0, 0, 0, 1, 1, 0}, //display number 3
+                              {1, 0, 0, 1, 1, 0, 0}, //display number 4
+                              {0, 1, 0, 0, 1, 0, 0}, //display number 5
+                              {0, 1, 0, 0, 0, 0, 0}, //display number 6
+                              {0, 0, 0, 1, 1, 1, 1}, //display number 7
+                              {0, 0, 0, 0, 0, 0, 0}, //display number 8
+                              {0, 0, 0, 0, 1, 0, 0}, //display number 9
+};
+
+void setup() {
+
+  pinMode(segmentA, OUTPUT);
+  pinMode(segmentB, OUTPUT);
+  pinMode(segmentC, OUTPUT);
+  pinMode(segmentD, OUTPUT);
+  pinMode(segmentE, OUTPUT);
+  pinMode(segmentF, OUTPUT);
+  pinMode(segmentG, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+
+  for (j = 0; j<7; j++)
+  {
+    digitalWrite(Arduino_Pins[j], Segment_Display[i][j]);
+  }
+
+
+  // button is pushed, number increase
+  if(digitalRead(BUTTON_PIN)==0)
+  {
+    while(digitalRead(BUTTON_PIN)==0)
+    {
+      
+    }
+    i++; 
+  }
+  
+  // reset number
+  if(i == 10)
+  {
+    i = 0;
+  }
+  delay(100);
+}
